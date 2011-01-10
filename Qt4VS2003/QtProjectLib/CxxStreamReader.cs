qt-labs/vstools

commit 4827cba4d29c9a8ce1cfccad35854460f9bbdc72

fix out of range error in CxxStreamReader
A line ending with a slash triggered this error.

Qt4VS2003/QtProjectLib/CxxStreamReader.cs
@@ -76,7 +76,7 @@ namespace Nokia.QtProjectLib
                     {
                         string lineCopy = line;
                         line = "";
-                        for (int i = 0, j = 1; i < lineCopy.Length; ++i, ++j)
+                        for (int i = 0, j = 1; j < lineCopy.Length; ++i, ++j)
                         {
                             if (lineCopy[i] == '/')
                             {

commit b55a2a2758b9a57fafb45298bd25310610f3d1db

fix regression introduced by 4827cba4
Fix redone.

Qt4VS2003/QtProjectLib/CxxStreamReader.cs
@@ -76,9 +76,9 @@ namespace Nokia.QtProjectLib
                     {
                         string lineCopy = line;
                         line = "";
-                        for (int i = 0, j = 1; j < lineCopy.Length; ++i, ++j)
+                        for (int i = 0, j = 1; i < lineCopy.Length; ++i, ++j)
                         {
-                            if (lineCopy[i] == '/')
+                            if (lineCopy[i] == '/' && j < lineCopy.Length)
                             {
                                 if (lineCopy[j] == '*')
                                 {