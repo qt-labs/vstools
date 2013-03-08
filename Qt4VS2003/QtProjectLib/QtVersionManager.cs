qt-labs/vstools

commit 997b0345662966f889fe909c00107492aa25a412

Save project's Qt version in ChangeQtVersion
In order to get rid of platform magic, that has
to be done after changing the project's Qt version
everything is now done in QtProject.ChangeQtVersion

Reviewed-by: joerg

Qt4VS2003/QtProjectLib/QtVersionManager.cs
@@ -244,9 +244,12 @@ namespace Nokia.QtProjectLib
             if (!IsVersionAvailable(version) && version != "$(DefaultQtVersion)")
                 return false;
             string key = "QtVersion " + platform;
-            project.Globals[key] = version;
-            if (!project.Globals.get_VariablePersists(key))
-                project.Globals.set_VariablePersists(key, true);
+            if (!project.Globals.get_VariableExists(key) || project.Globals[key].ToString() != version)
+            {
+                project.Globals[key] = version;
+                if (!project.Globals.get_VariablePersists(key))
+                    project.Globals.set_VariablePersists(key, true);
+            }
             return true;
         }
 

Qt4VS2003/Qt4VSAddin/AddInEventHandler.cs
@@ -485,13 +485,9 @@ namespace Qt4VSAddin
                     return;
                 }
                 bool newProjectCreated = false;
-                if (qtpro.ChangeQtVersion(qtVersion, qtVersionForPlatform, ref newProjectCreated))
-                {
-                    if (newProjectCreated)
-                        project = qtpro.Project;
-                    versionManager.SaveProjectQtVersion(project, qtVersionForPlatform);
-                }
-                qtVersion = qtVersionForPlatform;
+                bool versionChanged = qtpro.ChangeQtVersion(qtVersion, qtVersionForPlatform, ref newProjectCreated);
+                if (versionChanged && newProjectCreated)
+                    project = qtpro.Project;
             }
 
             if (!QtVSIPSettings.GetDisableAutoMocStepsUpdate())

Qt4VS2003/Qt4VSAddin/Connect.cs
@@ -304,8 +304,7 @@ namespace Qt4VSAddin
 
                                             QtProject qtProject = QtProject.Create(project);
                                             bool newProjectCreated = false;
-                                            if (qtProject.ChangeQtVersion(OldQtVersion, newQtVersion, ref newProjectCreated))
-                                                vManager.SaveProjectQtVersion(qtProject.Project, newQtVersion);
+                                            qtProject.ChangeQtVersion(OldQtVersion, newQtVersion, ref newProjectCreated);
                                         }
                                     }
                                     vManager.SaveSolutionQtVersion(_applicationObject.Solution, newQtVersion);

Qt4VS2003/Qt4VSAddin/QtSettings.cs
@@ -114,22 +114,9 @@ namespace Qt4VSAddin
             if (oldQtVersion != newQtVersion)
             {
                 bool newProjectCreated = false;
-                bool success = qtPro.ChangeQtVersion(oldQtVersion, newQtVersion, ref newProjectCreated);
-                if (success)
-                {
-                    if (newProjectCreated)
-                        project = qtPro.Project;
-
-                    VersionInformation newVersionInfo = versionManager.GetVersionInfo(newQtVersion);
-                    string newPlatform;
-                    newPlatform = newVersionInfo.GetVSPlatformName();
-                    versionManager.SaveProjectQtVersion(project, newQtVersion, newPlatform);
-                }
-                else
-                {
-                    versionManager.SaveProjectQtVersion(project, oldQtVersion);
-                    Messages.DisplayErrorMessage(SR.GetString("CannotChangeQtVersion"));
-                }
+                bool versionChanged = qtPro.ChangeQtVersion(oldQtVersion, newQtVersion, ref newProjectCreated);
+                if (versionChanged && newProjectCreated)
+                    project = qtPro.Project;
             }
         }
 

Qt4VS2003/QtProjectLib/QtProject.cs
@@ -3089,6 +3089,7 @@ namespace Nokia.QtProjectLib
             }
             catch
             {
+                Messages.DisplayErrorMessage(SR.GetString("CannotChangeQtVersion"));
                 return false;
             }
 
@@ -3107,6 +3108,7 @@ namespace Nokia.QtProjectLib
             HelperFunctions.SetEnvironmentVariable("QTDIR", viNew.qtDir);
 
             UpdateModules(viOld, viNew);
+            versionManager.SaveProjectQtVersion(envPro, newVersion, vsPlatformNameNew);
             return true;
         }
 

commit fca3a6f572876075b65b04dde35add4cde8be091

Fix for saving Qt5Version info to project globals.
When creating Qt Add-In project there is this 'Qt5Version' variable created
to project globals and set as persist. When converting project to QMake
generated one, this variable is not removed but persist is just set to false.

Now when trying to convert Qmake type project again to Qt Add-In this fails
because in changeset 997b0345 is a modification done which just checks the
existence of variable, not if it persists. And because of this persist is never
set and Add-In logic doesn't work correct anymore.

Adding persist check will fix the problem.

Old changeset 997b0345:

    diff --git a/Qt4VS2003/QtProjectLib/QtVersionManager.cs b/Qt4VS2003/QtProjectLib/QtVersionManager.cs
    index 6444b3f..58fd479 100644
    --- a/Qt4VS2003/QtProjectLib/QtVersionManager.cs
    +++ b/Qt4VS2003/QtProjectLib/QtVersionManager.cs
    @@ -244,9 +244,12 @@ namespace Nokia.QtProjectLib
        if (!IsVersionAvailable(version) && version != "$(DefaultQtVersion)")
            return false;
        string key = "QtVersion " + platform;
    -   project.Globals[key] = version;
    -   if (!project.Globals.get_VariablePersists(key))
    -       project.Globals.set_VariablePersists(key, true);
    +   if (!project.Globals.get_VariableExists(key) || project.Globals[key].ToString() != version)
    +   {
    +       project.Globals[key] = version;
    +       if (!project.Globals.get_VariablePersists(key))
    +           project.Globals.set_VariablePersists(key, true);
    +   }
        return true;
    }

Change-Id: I2a9aeb12fe05c2ed3ee37681eb672d990337ada8
Reviewed-by: Oliver Wolff <oliver.wolff@digia.com>
Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>

Qt4VS2003/QtProjectLib/QtVersionManager.cs
@@ -318,11 +318,9 @@ namespace Digia.Qt5ProjectLib
                 return false;
             string key = "Qt5Version " + platform;
             if (!project.Globals.get_VariableExists(key) || project.Globals[key].ToString() != version)
-            {
                 project.Globals[key] = version;
-                if (!project.Globals.get_VariablePersists(key))
-                    project.Globals.set_VariablePersists(key, true);
-            }
+            if (!project.Globals.get_VariablePersists(key))
+                project.Globals.set_VariablePersists(key, true);
             return true;
         }
 