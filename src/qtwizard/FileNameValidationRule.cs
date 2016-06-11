61bee84dde6b7e37e047d2bcb32669a59064e605
Karsten Heimrich <karsten.heimrich@theqtcompany.com>
2016-06-11 21:05:13 +0200

    Implement common functionality to be reused by project wizard's.
    Change-Id: I016117fef4efd2f69b6cbafd874991afcd7a459f
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

---
+++ src/qtwizard/FileNameValidationRule.cs
@@ -0,0 +1,55 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt VS Tools.
+**
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace QtProjectWizard
+{
+    public class FileNameValidationRule : VCLanguageManagerValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string) {
+                var filename = value as string;
+                if (FileExt == @".ui" || FileExt == @".qrc") {
+                    filename = filename.ToLower().Replace(@".h", @".x");
+                    filename = filename.Replace(FileExt, @".h");
+                }
+
+                if (Vclm.ValidateFileName(filename)
+                    && !Path.GetInvalidFileNameChars().Any(filename.Contains)) {
+                    return ValidationResult.ValidResult;
+                }
+            }
+            return new ValidationResult(false, @"Invalid file name.");
+        }
+    }
+}
