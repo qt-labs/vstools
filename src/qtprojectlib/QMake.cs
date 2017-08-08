38af7c6bcda2b306e3f50ab9a3690e5e03af0a83
Miguel Costa <miguel.costa@qt.io>
2017-08-08 13:02:16 +0200

    Set vcvars before calling qmake
    Instead of calculating the path to the compiler, calls the vcvars batch
    file and adds the environment variables to the qmake process start
    information. This ensures that qmake will have all available details
    about the compiler.
    
    Change-Id: Ie2f82851c57677ddc8bd186a920585e591122fe5
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

--- src/qtprojectlib/QMake.cs
+++ src/qtprojectlib/QMake.cs
@@ -33,6 +33,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace QtProjectLib
 {
@@ -88,38 +90,13 @@ namespace QtProjectLib
 
             qmakeProcess = CreateQmakeProcess(qmakeArgs, qtVersionInformation.qtDir + "\\bin\\qmake", fi.DirectoryName);
 
-            string clPath = GetClPath();
-            if (string.IsNullOrEmpty(clPath)) {
-                InvokeExternalTarget(PaneMessageDataEvent, "--- (Import): Unable to obtain path to cl");
-            } else {
-                string envPath = "";
-                if (!qmakeProcess.StartInfo.EnvironmentVariables.ContainsKey("PATH"))
-                    InvokeExternalTarget(PaneMessageDataEvent, "--- (Import): Warning: Empty PATH environment variable");
-                else
-                    envPath = qmakeProcess.StartInfo.EnvironmentVariables["PATH"];
-
-                qmakeProcess.StartInfo.EnvironmentVariables["PATH"] = clPath;
-                if (!string.IsNullOrEmpty(envPath))
-                    qmakeProcess.StartInfo.EnvironmentVariables["PATH"] += ";" + envPath;
-            }
+            if (!SetVCVars(qmakeProcess.StartInfo))
+                InvokeExternalTarget(PaneMessageDataEvent, "--- (Import): Error setting VC vars");
 
             // We must set the QTDIR environment variable, because we're clearing QMAKE_LIBDIR_QT above.
             // If we do not set this, the Qt libraries will be QtCored.lib instead of QtCore4d.lib even
             // for shared builds.
             qmakeProcess.StartInfo.EnvironmentVariables["QTDIR"] = qtVersionInformation.qtDir;
-
-            // determine which vs version we are currently using and inform qmake about it
-            var regPath = dteObject.Application.RegistryRoot + "\\Setup\\VC";
-            var key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(regPath);
-            if (key != null) {
-                var keyValue = key.GetValue("ProductDir", "").ToString();
-                var envVar = qmakeProcess.StartInfo.EnvironmentVariables["path"];
-                if (envVar != null) {
-                    var value = envVar + ";" + keyValue;
-                    qmakeProcess.StartInfo.EnvironmentVariables["path"] = value;
-                } else
-                    qmakeProcess.StartInfo.EnvironmentVariables.Add("path", keyValue);
-            }
 
             try {
                 InvokeExternalTarget(PaneMessageDataEvent, "--- (qmake) : Using: " + qmakeProcess.StartInfo.FileName);
@@ -262,41 +239,129 @@ namespace QtProjectLib
             }
         }
 
-        private string GetClPath()
+        private string GetVCPath()
         {
-            bool x64 = QtVersionManager.The().GetVersionInfo(
-                QtVersionManager.The().GetDefaultVersion()).is64Bit();
 #if VS2017
             string vsPath = GetRegistrySoftwareString(@"Microsoft\VisualStudio\SxS\VS7", "15.0");
             if (string.IsNullOrEmpty(vsPath))
                 return "";
-
-            string vcVarsDefaultConfigFile = Path.Combine(vsPath,
-                @"VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt");
-            string vcVarsToolsVersion = "";
-            try {
-                using (var file = new StreamReader(vcVarsDefaultConfigFile)) {
-                    vcVarsToolsVersion = file.ReadLine();
-                    if (String.IsNullOrEmpty(vcVarsToolsVersion))
-                        return "";
-                }
-            } catch (Exception) {
-                return "";
-            }
-
-            string clPath = Path.Combine(vsPath, string.Format(@"VC\Tools\MSVC\{0}\bin\{1}",
-                vcVarsToolsVersion.Trim(), (x64 ? @"HostX64\x64" : @"HostX86\x86")));
+            string vcPath = Path.Combine(vsPath, "VC");
 #elif VS2015
             string vcPath = GetRegistrySoftwareString(@"Microsoft\VisualStudio\SxS\VC7", "14.0");
             if (string.IsNullOrEmpty(vcPath))
                 return ""; //could not get registry key
-
-            string clPath = Path.Combine(vcPath, (x64 ? @"bin\amd64" : "bin"));
+#elif VS2013
+            string vcPath = GetRegistrySoftwareString(@"Microsoft\VisualStudio\SxS\VC7", "12.0");
+            if (string.IsNullOrEmpty(vcPath))
+                return ""; //could not get registry key
 #endif
-            if (!Directory.Exists(clPath))
-                return "";
+            return vcPath;
+        }
 
-            return clPath;
+        private bool SetVCVars(ProcessStartInfo startInfo)
+        {
+            bool isOS64Bit = System.Environment.Is64BitOperatingSystem;
+            bool isQt64Bit = QtVersionManager.The().GetVersionInfo(
+                QtVersionManager.The().GetDefaultVersion()).is64Bit();
+
+            string vcPath = GetVCPath();
+            if (vcPath == "")
+                return false;
+
+            string comspecPath = Environment.GetEnvironmentVariable("COMSPEC");
+#if VS2017
+            string vcVarsCmd = "";
+            string vcVarsArg = "";
+            if (isOS64Bit && isQt64Bit)
+                vcVarsCmd = Path.Combine(vcPath, @"Auxiliary\Build\vcvars64.bat");
+            else if (!isOS64Bit && !isQt64Bit)
+                vcVarsCmd = Path.Combine(vcPath, @"Auxiliary\Build\vcvars32.bat");
+            else if (isOS64Bit && !isQt64Bit)
+                vcVarsCmd = Path.Combine(vcPath, @"Auxiliary\Build\vcvarsamd64_x86.bat");
+            else if (!isOS64Bit && isQt64Bit)
+                vcVarsCmd = Path.Combine(vcPath, @"Auxiliary\Build\vcvarsx86_amd64.bat");
+#elif VS2015 || VS2013
+            string vcVarsCmd = Path.Combine(vcPath, "vcvarsall.bat");
+            string vcVarsArg = "";
+            if (isOS64Bit && isQt64Bit)
+                vcVarsArg = "amd64";
+            else if (!isOS64Bit && !isQt64Bit)
+                vcVarsArg = "x86";
+            else if (isOS64Bit && !isQt64Bit)
+                vcVarsArg = "amd64_x86";
+            else if (!isOS64Bit && isQt64Bit)
+                vcVarsArg = "x86_amd64";
+#endif
+            const string markSTX = ":@:@:@";
+            const string markEOL = ":#:#:#";
+            string command = string.Format("/c \"{0}\" {1} && echo {2} && set", vcVarsCmd, vcVarsArg, markSTX);
+            var vcVarsStartInfo = new ProcessStartInfo(comspecPath, command);
+            vcVarsStartInfo.CreateNoWindow = true;
+            vcVarsStartInfo.UseShellExecute = false;
+            vcVarsStartInfo.RedirectStandardError = true;
+            vcVarsStartInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(vcVarsStartInfo);
+            StringBuilder stdOut = new StringBuilder();
+
+            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+                stdOut.AppendFormat("{0}\n{1}\n", e.Data, markEOL);
+            process.BeginOutputReadLine();
+
+            process.WaitForExit();
+            bool ok = (process.ExitCode == 0);
+            process.Close();
+            if (!ok)
+                return false;
+
+            SortedDictionary<string, List<string>> vcVars = new SortedDictionary<string, List<string>>();
+            string[] split = stdOut.ToString().Split(new string[] { "\n", "=", ";" }, StringSplitOptions.None);
+            int i = 0;
+            for (; i < split.Length && split[i].Trim() != markSTX; i++) {
+                //Skip to start of data
+            }
+            i++; //Advance to next item
+            for (; i < split.Length && split[i].Trim() != markEOL; i++) {
+                //Skip to end of line
+            }
+            i++; //Advance to next item
+            for (; i < split.Length; i++) {
+                //Process first item (variable name)
+                string key = split[i].ToUpper().Trim();
+                i++; //Advance to next item
+                List<string> vcVarValue = vcVars[key] = new List<string>();
+                for (; i < split.Length && split[i].Trim() != markEOL; i++) {
+                    //Process items up to end of line (variable value(s))
+                    vcVarValue.Add(split[i].Trim());
+                }
+            }
+
+            foreach (var vcVar in vcVars) {
+                if (vcVar.Value.Count == 1) {
+                    startInfo.EnvironmentVariables[vcVar.Key] = vcVar.Value[0];
+                } else {
+                    if (!startInfo.EnvironmentVariables.ContainsKey(vcVar.Key)) {
+                        foreach (var vcVarValue in vcVar.Value) {
+                            if (!string.IsNullOrWhiteSpace(vcVarValue)) {
+                                startInfo.EnvironmentVariables[vcVar.Key] += vcVarValue + ";";
+                            }
+                        }
+                    } else {
+                        string[] startInfoVariableValues = startInfo.EnvironmentVariables[vcVar.Key]
+                            .Split(new string[] { ";" }, StringSplitOptions.None);
+                        foreach (var vcVarValue in vcVar.Value) {
+                            if (!string.IsNullOrWhiteSpace(vcVarValue)
+                                && !startInfoVariableValues.Any(s => s.Trim().Equals(vcVarValue, StringComparison.OrdinalIgnoreCase))) {
+                                if (!startInfo.EnvironmentVariables[vcVar.Key].EndsWith(";"))
+                                    startInfo.EnvironmentVariables[vcVar.Key] += ";";
+                                startInfo.EnvironmentVariables[vcVar.Key] += vcVarValue + ";";
+                            }
+                        }
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
