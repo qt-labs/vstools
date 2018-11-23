1ffd6fc620195523dc29173780cc92373454928a
Miguel Costa <miguel.costa@qt.io>
2018-11-23 18:07:43 +0100

    Automate change of version number
    Before this patch, there were 40+ different locations in the source
    files where the version number was stored. The version number is now
    defined in a single source file as the property $(QtVSToolsVersion).
    Other sources that use the version string are then generated from
    corresponding templates using the template engine included in Visual
    Studio (Text Template Transformation Toolkit, or T4). Projects that
    integrate with the Qt VS Tools will also have access to the version
    definition property.
    
    Change-Id: Ib3a372c6f0e0c446eef98b4fa26b58e1565bb025
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

--- src/qtvstools/Properties/AssemblyInfo.cs
+++ src/qtvstools/Properties/AssemblyInfo.cs
@@ -24,7 +24,11 @@
 **
 ** $QT_END_LICENSE$
 **
-****************************************************************************/
+****************************************************************************
+<#@output extension="tt.cs" #>
+<#@include file="$(SolutionDir)\version.tt" #>
+**              <#=WARNING_GENERATED_FILE#>
+***************************************************************************/
 
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -59,5 +63,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.1.0")]
-[assembly: AssemblyFileVersion("2.2.1.0")]
+[assembly: AssemblyVersion("<#=QT_VS_TOOLS_VERSION_ASSEMBLY#>")]
+[assembly: AssemblyFileVersion("<#=QT_VS_TOOLS_VERSION_ASSEMBLY_FILE#>")]
