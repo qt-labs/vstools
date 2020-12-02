c3fb4fbbbfe26d68c60810402311bda3a08f00bd
Miguel Costa <miguel.costa@qt.io>
2020-12-02 10:53:17 +0100

    Add options to vstools.bat
    Added the following new options to the vstools.bat script:
    
     -install                  Install the VSIX package after the build
                               is completed (uses the VSIX installer UI)
    
     -transform_incremental    Only process T4 source templates that have
                               been modified (by default, all templates are
                               processed, even in an incremental build)
    
    Change-Id: Ia4238a69badd5533a0c5a0c16998c81e03e2046b
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

--- src/vstools.bat
+++ src/vstools.bat
@@ -4,7 +4,8 @@ SETLOCAL
 SET SCRIPT=%~n0
 
 SET "USAGE=%SCRIPT%"
-SET "USAGE=%USAGE% [ -init ^| [-rebuild] [-deploy ^<DEPLOY_DIR^>] ]"
+SET "USAGE=%USAGE% [ -init ^| [-rebuild] [-deploy ^<DEPLOY_DIR^>] [-install] ]"
+SET "USAGE=%USAGE% [-transform_incremental]"
 SET "USAGE=%USAGE% [-vs2019]"
 SET "USAGE=%USAGE% [-vs2017]"
 SET "USAGE=%USAGE% [-vs2015]"
@@ -28,6 +29,12 @@ SET VERBOSE=%FALSE%
 SET REBUILD=%FALSE%
 SET INIT=%FALSE%
 SET BINARYLOG=%FALSE%
+SET DO_INSTALL=%FALSE%
+SET TRANSFORM_INCREMENTAL=false
+
+SET FLAG_VS2019=-vs2019
+SET FLAG_VS2017=-vs2017
+SET FLAG_VS2015=-vs2015
 
 :parseArgs
 IF NOT "%1"=="" (
@@ -38,25 +45,29 @@ IF NOT "%1"=="" (
     ) ELSE IF "%1"=="-deploy" (
         SET QtVSToolsDeployTarget=%~f2
         SHIFT
+    ) ELSE IF "%1"=="-install" (
+        SET DO_INSTALL=%TRUE%
+    ) ELSE IF "%1"=="-transform_incremental" (
+        SET TRANSFORM_INCREMENTAL=true
     ) ELSE IF "%1"=="-verbose" (
         SET VERBOSE=%TRUE%
     ) ELSE IF "%1"=="-bl" (
         SET BINARYLOG=%TRUE%
-    ) ELSE IF "%1"=="-vs2019" (
+    ) ELSE IF "%1"=="%FLAG_VS2019%" (
         SET VS_VERSIONS=%VS_VERSIONS%,%VS2019%
         SET VS_LATEST=%VS2019%
-        SET VS2019=
         SET VS_VERSIONS_DEFAULT=%FALSE%
-    ) ELSE IF "%1"=="-vs2017" (
+        SET FLAG_VS2019=
+    ) ELSE IF "%1"=="%FLAG_VS2017%" (
         SET VS_VERSIONS=%VS_VERSIONS%,%VS2017%
         SET VS_LATEST=%VS2017%
-        SET VS2017=
         SET VS_VERSIONS_DEFAULT=%FALSE%
-    ) ELSE IF "%1"=="-vs2015" (
+        SET FLAG_VS2017=
+    ) ELSE IF "%1"=="%FLAG_VS2015%" (
         SET VS_VERSIONS=%VS_VERSIONS%,%VS2015%
         SET VS_LATEST=%VS2015%
-        SET VS2015=
         SET VS_VERSIONS_DEFAULT=%FALSE%
+        SET FLAG_VS2015=
     ) ELSE IF "%1"=="-help" (
         GOTO :usage
     ) ELSE (
@@ -72,9 +83,12 @@ IF %INIT% (
     SET MSBUILD_EXTRAS=%MSBUILD_EXTRAS% /p:CleanDependsOn=TransformDuringBuild
     SET VS_VERSIONS=%VS_ALL%
     SET VS_VERSIONS=%VS_LATEST%
+    SET DO_INSTALL=%FALSE%
+    SET TRANSFORM_INCREMENTAL=false
 ) ELSE IF %REBUILD% (
     SET MSBUILD_TARGETS=Rebuild
     IF %VS_VERSIONS_DEFAULT% SET VS_VERSIONS=%VS_ALL%
+    SET TRANSFORM_INCREMENTAL=false
 ) ELSE (
     SET MSBUILD_TARGETS=Build
     IF %VS_VERSIONS_DEFAULT% SET VS_VERSIONS=%VS_ALL%
@@ -110,7 +124,7 @@ FOR %%v IN (%VS_VERSIONS%) DO (
             IF %VERBOSE% ECHO ## CALL "%%p\VC\vcvarsall.bat" x86 8.1
             CALL "%%p\VC\vcvarsall.bat" x86 8.1 > NUL
         ) ELSE (
-            ECHO "Error running VC Vars script" 1>&2
+            ECHO Error running VC Vars script 1>&2
             EXIT /B 2
         )
 
@@ -119,15 +133,29 @@ FOR %%v IN (%VS_VERSIONS%) DO (
             /maxCpuCount ^
             /p:Configuration=Release ^
             /p:Platform="Any CPU" ^
-            /p:TransformOutOfDateOnly=false ^
+            /p:TransformOutOfDateOnly=%TRANSFORM_INCREMENTAL% ^
             /t:%MSBUILD_TARGETS% ^
             %MSBUILD_EXTRAS% ^
             QtVSTools.sln
 
         IF NOT "%ERRORLEVEL%"=="0" (
-            ECHO "Error building solution" 1>&2
+            ECHO Error building solution 1>&2
             EXIT /B %ERRORLEVEL%
         )
+
+        IF %DO_INSTALL% (
+            ECHO Removing previous installation...
+            IF "%%~v"=="%VS2019:"=%" (
+                VSIXInstaller /uninstall:QtVsTools.bf3c71c0-ab41-4427-ada9-9b3813d89ff5
+            ) ELSE IF "%%~v"=="%VS2017:"=%" (
+                VSIXInstaller /uninstall:QtVsTools.13121978-cd02-4fd0-89bd-e36f85abe16a
+            ) ELSE IF "%%~v"=="%VS2015:"=%" (
+                VSIXInstaller /uninstall:QtVsTools.30112013-cd02-4fd0-89bd-e36f85abe16a
+            )
+            ECHO Installing...
+            VSIXInstaller qtvstools\bin\Release\QtVsTools.vsix
+        )
+
         ENDLOCAL
     )
 )
