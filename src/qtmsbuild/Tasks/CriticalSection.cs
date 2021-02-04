60a69e7faa8c98e4ba75dece29d95f270abbe3b0
Miguel Costa <miguel.costa@qt.io>
2021-02-04 16:08:04 +0100

    Fix build concurrency issues
    The Qt/MSBuild targets will now run inside a critical section to ensure
    serialized execution. This fixes a bug where parallel builds of the
    same project would fail intermittently due to concurrent file accesses.
    
    The scenario where this issue is most relevant is when building,
    in parallel, projects that reference at least one common Qt project in
    the same solution. Targets of the referenced project will be triggered
    in the context of the parallel builds of the referencing projects.
    This then leads to concurrent file access errors (6% build fail rate
    with a sample solution containing one referenced project and two
    referencing projects).
    
    Only the concurrent execution of build targets from the same project
    is synchronized. Concurrent builds of different, unrelated projects
    will still be executed in parallel without any negative impact in
    build performance.
    
    Task-number: QTVSADDINBUG-857
    Change-Id: Iaf1e9f800f6fc2d6bd956cfef61907e6f9fade81
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

---
+++ src/qtmsbuild/Tasks/CriticalSection.cs
@@ -0,0 +1,105 @@
+﻿/****************************************************************************
+**
+** Copyright (C) 2021 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt VS Tools.
+**
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#region Task TaskName="CriticalSection"
+
+#region Using
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using Microsoft.Build.Framework;
+using Microsoft.Win32;
+#endregion
+
+#region Comment
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/// TASK CriticalSection
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Enter or leave a critical section during build
+// Parameters:
+//      in bool   Lock: 'true' when entering the critical section, 'false' when leaving
+//      in string Name: Critical section lock name
+#endregion
+
+namespace QtVsTools.QtMsBuild.Tasks
+{
+    public static class CriticalSection
+    {
+        public static Microsoft.Build.Utilities.TaskLoggingHelper Log { get; set; }
+        public static IBuildEngine BuildEngine { get; set; }
+
+        public static bool Execute(
+        #region Parameters
+            System.Boolean Lock,
+            System.String Name)
+        #endregion
+        {
+            #region Code
+            var buildEngine = BuildEngine as IBuildEngine4;
+
+            // Acquire lock
+            string lockName = string.Format("Global\\{0}", Name);
+            EventWaitHandle buildLock = null;
+            if (!EventWaitHandle.TryOpenExisting(lockName, out buildLock)) {
+                // Lock does not exist; create lock
+                bool lockCreated;
+                buildLock = new EventWaitHandle(
+                    true, EventResetMode.AutoReset, lockName, out lockCreated);
+                if (lockCreated) {
+                    // Keep lock alive until end of build
+                    buildEngine.RegisterTaskObject(
+                        Name, buildLock, RegisteredTaskObjectLifetime.Build, false);
+                }
+            }
+            if (buildLock == null) {
+                Log.LogError("Qt::BuildLock[{0}]: Error accessing lock", Name);
+                return false;
+            }
+            if (Lock) {
+                // Wait until locked
+                while (!buildLock.WaitOne(3000)) {
+                    // Check every 3 secs. for build errors
+                    if (Log.HasLoggedErrors) {
+                        Log.LogError("Qt::BuildLock[{0}]: Errors logged; wait aborted", Name);
+                        return false;
+                    }
+                    // Issue "still waiting" warning
+                    Log.LogWarning("Qt::BuildLock[{0}]: Waiting...", Name);
+                }
+            } else {
+                // Unlock
+                buildLock.Set();
+            }
+            #endregion
+
+            return true;
+        }
+    }
+}
+#endregion
