ae3fac58a43cb74f2ad2587d7db3bd9888a2cf60
Miguel Costa <miguel.costa@qt.io>
2021-03-11 16:06:14 +0100

    Run translation tools in legacy projects
    Fixed a regression issue introduced in v2.7.1 where Qt translation tools
    would not work with older formats of Qt VS projects (i.e. those still
    using custom-build items for moc, etc.)
    
    Task-number: QTVSADDINBUG-871
    Change-Id: Ie6337fcf97df3be5289f54d68aef2c4f03fdbef9
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

--- src/qtvstools/Translation.cs
+++ src/qtvstools/Translation.cs
@@ -43,6 +43,7 @@ using System.Windows.Forms;
 
 namespace QtVsTools
 {
+    using static Core.HelperFunctions;
     using static QtMsBuild.QtProjectTracker;
     using Property = KeyValuePair<string, string>;
 
@@ -109,36 +110,51 @@ namespace QtVsTools
             EnvDTE.Project project,
             IEnumerable<string> selectedFiles = null)
         {
-            var qtPro = QtProject.Create(project);
-            if (project == null || qtPro == null) {
-                Messages.Print(
-                    "translation: Error accessing project interface");
-                return;
-            }
+            using (WaitDialog.Start(
+                "Qt Visual Studio Tools", "Running translation tool...")) {
 
-            var activeConfig = project.ConfigurationManager?.ActiveConfiguration;
-            if (activeConfig == null) {
-                Messages.Print(
-                    "translation: Error accessing build interface");
-                return;
-            }
-            var activeConfigId = string.Format("{0}|{1}",
-                activeConfig.ConfigurationName, activeConfig.PlatformName);
+                var qtPro = QtProject.Create(project);
+                if (project == null || qtPro == null) {
+                    Messages.Print(
+                        "translation: Error accessing project interface");
+                    return;
+                }
 
-            var target = "QtTranslation";
-            var properties = new List<Property>();
-            switch (buildAction) {
-                case BuildAction.Update:
-                    properties.Add(PROPERTY("QtTranslationForceUpdate", "true"));
-                    break;
-                case BuildAction.Release:
-                    properties.Add(PROPERTY("QtTranslationForceRelease", "true"));
-                    break;
-            }
-            if (selectedFiles != null)
-                properties.Add(PROPERTY("SelectedFiles", string.Join(";", selectedFiles)));
+                if (qtPro.FormatVersion < Resources.qtMinFormatVersion_Settings) {
+                    Messages.Print("translation: Legacy project format");
+                    try {
+                        Legacy_RunTranslation(buildAction, qtPro, selectedFiles);
+                    } catch (Exception e) {
+                        Messages.Print(
+                            e.Message + "\r\n\r\nStacktrace:\r\n" + e.StackTrace);
+                    }
+                    return;
+                }
 
-            Build(project, activeConfigId, properties.ToArray(), target);
+                var activeConfig = project.ConfigurationManager?.ActiveConfiguration;
+                if (activeConfig == null) {
+                    Messages.Print(
+                        "translation: Error accessing build interface");
+                    return;
+                }
+                var activeConfigId = string.Format("{0}|{1}",
+                    activeConfig.ConfigurationName, activeConfig.PlatformName);
+
+                var target = "QtTranslation";
+                var properties = new List<Property>();
+                switch (buildAction) {
+                    case BuildAction.Update:
+                        properties.Add(PROPERTY("QtTranslationForceUpdate", "true"));
+                        break;
+                    case BuildAction.Release:
+                        properties.Add(PROPERTY("QtTranslationForceRelease", "true"));
+                        break;
+                }
+                if (selectedFiles != null)
+                    properties.Add(PROPERTY("SelectedFiles", string.Join(";", selectedFiles)));
+
+                Build(project, activeConfigId, properties.ToArray(), target);
+            }
         }
 
         public static void RunlUpdate(EnvDTE.Solution solution)
@@ -148,6 +164,98 @@ namespace QtVsTools
 
             foreach (var project in HelperFunctions.ProjectsInSolution(solution.DTE))
                 RunlUpdate(project);
+        }
+
+        static void Legacy_RunTranslation(
+            BuildAction buildAction,
+            QtProject qtProject,
+            IEnumerable<string> tsFiles)
+        {
+            if (tsFiles == null) {
+                tsFiles = (qtProject.VCProject
+                    .GetFilesEndingWith(".ts") as IVCCollection)
+                    .Cast<VCFile>()
+                    .Select(vcFile => vcFile.RelativePath);
+                if (tsFiles == null) {
+                    Messages.Print("translation: no translation files found");
+                    return;
+                }
+            }
+            string tempFile = null;
+            foreach (var file in tsFiles.Where(file => file != null))
+                Legacy_RunTranslation(buildAction, qtProject, file, ref tempFile);
+        }
+
+        static void Legacy_RunTranslation(
+            BuildAction buildAction,
+            QtProject qtProject,
+            string tsFile,
+            ref string tempFile)
+        {
+            var qtVersion = qtProject.GetQtVersion();
+            var qtInstallPath = QtVersionManager.The().GetInstallPath(qtVersion);
+            if (string.IsNullOrEmpty(qtInstallPath)) {
+                Messages.Print("translation: Error accessing Qt installation");
+                return;
+            }
+
+            var procInfo = new ProcessStartInfo
+            {
+                WorkingDirectory = qtProject.ProjectDir,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                Arguments = ""
+            };
+            switch (buildAction) {
+                case BuildAction.Update:
+                    Messages.Print("\r\n--- (lupdate) file: " + tsFile);
+                    procInfo.FileName = Path.Combine(qtInstallPath, "bin", "lupdate.exe");
+                    var options = QtVSIPSettings.GetLUpdateOptions();
+                    if (!string.IsNullOrEmpty(options))
+                        procInfo.Arguments += options + " ";
+                    if (tempFile == null) {
+                        var inputFiles = GetProjectFiles(qtProject.Project, FilesToList.FL_HFiles)
+                            .Union(GetProjectFiles(qtProject.Project, FilesToList.FL_CppFiles))
+                            .Union(GetProjectFiles(qtProject.Project, FilesToList.FL_UiFiles));
+                        tempFile = Path.GetTempFileName();
+                        File.WriteAllLines(tempFile, inputFiles);
+                    }
+                    procInfo.Arguments += string.Format("\"@{0}\" -ts \"{1}\"", tempFile, tsFile);
+                    break;
+                case BuildAction.Release:
+                    Messages.Print("\r\n--- (lrelease) file: " + tsFile);
+                    procInfo.FileName = Path.Combine(qtInstallPath, "bin", "lrelease.exe");
+                    options = QtVSIPSettings.GetLReleaseOptions();
+                    if (!string.IsNullOrEmpty(options))
+                        procInfo.Arguments += options + " ";
+                    procInfo.Arguments += string.Format("\"{0}\"", tsFile);
+                    break;
+            }
+            using (var proc = Process.Start(procInfo)) {
+                proc.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                        Messages.Print(e.Data);
+                };
+                proc.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                        Messages.Print(e.Data);
+                };
+                proc.BeginOutputReadLine();
+                proc.BeginErrorReadLine();
+                proc.WaitForExit();
+                switch (proc.ExitCode) {
+                    case 0:
+                        Messages.Print("translation: ok");
+                        break;
+                    default:
+                        Messages.Print(string.Format("translation: ERROR {0}", proc.ExitCode));
+                        break;
+                }
+            }
         }
 
         public static void CreateNewTranslationFile(EnvDTE.Project project)
