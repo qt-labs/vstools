db326690ec282f06757cacce40710281122e31d8
Miguel Costa <miguel.costa@qt.io>
2021-05-11 11:51:23 +0200

    Add PriorityQueue type
    Added a new generic utility type, PriorityQueue, that represents a
    thread-safe collection of objects that can be retrieved by the order of
    their priority at enqueue. The underlying representation is based on a
    hash-table, mapping items to their priority, and a binary search tree
    containing the items sorted by priority. This results in the following
    time complexity profile:
     * Enqueue, Dequeue ≈ O(log n)
     * Peek, Contains ≈ O(1)
    
    Also added a specialized PunisherQueue, based on PriorityQueue, which
    behaves like the standard Queue<T> of the .NET framework, with the
    following main differences:
     * Thread-safe: can be simultaneously used by several threads.
     * Re-queue: Adding the same object (i.e. an object already present in
     the queue) many times will not create multiple entries in the queue for
     that object. Instead, at each call to Enqueue(), the object is moved
     from its current position to the back of the queue (i.e. "punished").
    
    Change-Id: I3e53db1d64190f0470edf645f20312a0e8976ba4
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

---
+++ src/qtvstools/Common/PriorityQueue.cs
@@ -0,0 +1,156 @@
+﻿/****************************************************************************
+**
+** Copyright (C) 2021 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt VS Tools.
+**
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QtVsTools
+{
+    public class PriorityQueue<T, TPriority> : BasePriorityQueue<T, TPriority>
+        where TPriority : IComparable<TPriority>
+    {
+        public PriorityQueue() : base()
+        { }
+
+        public PriorityQueue(Func<T, object> getItemKey) : base(getItemKey)
+        { }
+
+        public new void Enqueue(T item, TPriority priority)
+        {
+            base.Enqueue(item, priority);
+        }
+    }
+
+    public abstract class BasePriorityQueue<T, TPriority> : Concurrent, IEnumerable<T>
+        where TPriority : IComparable<TPriority>
+    {
+        SortedDictionary<TPriority, T> ItemsByPriority { get; set; }
+        Dictionary<object, TPriority> ItemPriority { get; set; }
+        T Head { get; set; }
+
+        IEnumerable<T> Items => ThreadSafe(() => ItemsByPriority.Values.ToList());
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => Items.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => Items.GetEnumerator();
+
+        Func<T, object> GetItemKey { get; set; }
+
+        public BasePriorityQueue()
+        {
+            Clear();
+            GetItemKey = (x => x);
+        }
+
+        public BasePriorityQueue(Func<T, object> getItemKey)
+        {
+            Clear();
+            GetItemKey = getItemKey;
+        }
+
+        public void Clear()
+        {
+            lock (CriticalSection) {
+                ItemsByPriority = new SortedDictionary<TPriority, T>();
+                ItemPriority = new Dictionary<object, TPriority>();
+                Head = default(T);
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            lock (CriticalSection) {
+                return ItemPriority.ContainsKey(GetItemKey(item));
+            }
+        }
+
+        // Base Enqueue() is protected to allow specialized implementations to
+        // hide the concept of priority (e.g. PunisherQueue).
+        //
+        protected void Enqueue(T item, TPriority priority)
+        {
+            if (item == null)
+                throw new InvalidOperationException("Item cannot be null.");
+            lock (CriticalSection) {
+                T oldItem;
+                if (ItemsByPriority.TryGetValue(priority, out oldItem) && !item.Equals(oldItem))
+                    throw new InvalidOperationException("An item with the same priority exists.");
+                TPriority oldPriority;
+                if (ItemPriority.TryGetValue(GetItemKey(item), out oldPriority))
+                    ItemsByPriority.Remove(oldPriority);
+                ItemPriority[GetItemKey(item)] = priority;
+                ItemsByPriority[priority] = item;
+                Head = ItemsByPriority.First().Value;
+            }
+        }
+
+        public bool TryPeek(out T result)
+        {
+            lock (CriticalSection) {
+                result = Head;
+                return ItemsByPriority.Any();
+            }
+        }
+
+        public T Peek()
+        {
+            lock (CriticalSection) {
+                T result;
+                if (!TryPeek(out result))
+                    throw new InvalidOperationException("Queue is empty.");
+                return result;
+            }
+        }
+
+        public bool TryDequeue(out T result)
+        {
+            lock (CriticalSection) {
+                result = Head;
+                if (!ItemsByPriority.Any())
+                    return false;
+                ItemsByPriority.Remove(ItemPriority[GetItemKey(result)]);
+                ItemPriority.Remove(GetItemKey(result));
+                if (ItemsByPriority.Any())
+                    Head = ItemsByPriority.First().Value;
+                else
+                    Head = default(T);
+                return true;
+            }
+        }
+
+        public T Dequeue()
+        {
+            lock (CriticalSection) {
+                T result;
+                if (!TryDequeue(out result))
+                    throw new InvalidOperationException("Queue is empty.");
+                return result;
+            }
+        }
+    }
+}
---
+++ src/tests/Test_QtVsTools.PriorityQueue/Test_PriorityQueue.cs
@@ -0,0 +1,219 @@
+﻿/****************************************************************************
+**
+** Copyright (C) 2021 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt VS Tools.
+**
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QtVsTools.Test.PriorityQueue
+{
+    [TestClass]
+    public class Test_PriorityQueue
+    {
+        [TestMethod]
+        public void TestEnqueueWithPriority()
+        {
+            var q = new PriorityQueue<string, int>();
+            q.Enqueue("c", 30);
+            q.Enqueue("a", 13);
+            q.Enqueue("d", 47);
+            q.Enqueue("b", 28);
+            Assert.IsTrue(string.Join("", q) == "abcd");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestEnqueueWithSamePriority()
+        {
+            var q = new PriorityQueue<string, int>();
+            q.Enqueue("a", 1);
+            q.Enqueue("a", 1);
+            Assert.IsTrue(string.Join("", q) == "a");
+            q.Enqueue("b", 1);
+        }
+
+        [TestMethod]
+        public void TestEnqueueContains()
+        {
+            var q = new PunisherQueue<string>();
+            q.Enqueue("a");
+            q.Enqueue("b");
+            q.Enqueue("c");
+            Assert.IsTrue(q.Contains("a"));
+            Assert.IsTrue(q.Contains("b"));
+            Assert.IsTrue(q.Contains("c"));
+            Assert.IsTrue(string.Join("", q) == "abc");
+        }
+
+        [TestMethod]
+        public void TestEnqueueTwice()
+        {
+            var q = new PunisherQueue<string>();
+            q.Enqueue("a");
+            q.Enqueue("b");
+            q.Enqueue("a");
+            q.Enqueue("c");
+            q.Enqueue("b");
+            Assert.IsTrue(string.Join("", q) == "acb");
+        }
+
+        [TestMethod]
+        public void TestTryPeek()
+        {
+            var q = new PunisherQueue<string>();
+            string s;
+            Assert.IsTrue(!q.TryPeek(out s));
+            q.Enqueue("a");
+            q.Enqueue("b");
+            q.Enqueue("c");
+            Assert.IsTrue(q.TryPeek(out s) && s == "a");
+            Assert.IsTrue(string.Join("", q) == "abc");
+        }
+
+        [TestMethod]
+        public void TestPeek()
+        {
+            var q = new PunisherQueue<string>();
+            q.Enqueue("a");
+            q.Enqueue("b");
+            q.Enqueue("c");
+            Assert.IsTrue(q.Peek() == "a");
+            Assert.IsTrue(string.Join("", q) == "abc");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestPeekEmpty()
+        {
+            var q = new PunisherQueue<string>();
+            q.Peek();
+        }
+
+        [TestMethod]
+        public void TestTryDequeue()
+        {
+            var q = new PunisherQueue<string>();
+            string s;
+            Assert.IsTrue(!q.TryDequeue(out s));
+            q.Enqueue("a");
+            q.Enqueue("b");
+            q.Enqueue("c");
+            Assert.IsTrue(q.TryDequeue(out s) && s == "a");
+            Assert.IsTrue(string.Join("", q) == "bc");
+        }
+
+        [TestMethod]
+        public void TestDequeue()
+        {
+            var q = new PunisherQueue<string>();
+            q.Enqueue("a");
+            q.Enqueue("b");
+            q.Enqueue("c");
+            Assert.IsTrue(q.Dequeue() == "a");
+            Assert.IsTrue(string.Join("", q) == "bc");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestDequeueEmpty()
+        {
+            var q = new PunisherQueue<string>();
+            q.Dequeue();
+        }
+
+        [TestMethod]
+        public void TestClear()
+        {
+            var q = new PunisherQueue<string>();
+            q.Enqueue("a");
+            q.Enqueue("b");
+            q.Enqueue("c");
+            q.Clear();
+            q.Enqueue("x");
+            q.Enqueue("y");
+            q.Enqueue("z");
+            Assert.IsTrue(string.Join("", q) == "xyz");
+        }
+
+        [TestMethod]
+        public void TestConcurrency()
+        {
+            var q = new PunisherQueue<string>();
+            int n = 0;
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 10000; ++i) {
+                    q.Enqueue(Path.GetRandomFileName());
+                    ++n;
+                    Thread.Yield();
+                }
+            });
+            for (int i = 0; i < 10000; ++i) {
+                string s;
+                if (!q.TryDequeue(out s))
+                    --i;
+                --n;
+                Thread.Yield();
+            }
+            if (n == 0)
+                Assert.Inconclusive();
+            Assert.IsTrue(q.Count() == 0);
+        }
+
+        [TestMethod]
+        public void TestGetItemKey()
+        {
+            var q = new PunisherQueue<string>(item =>
+            {
+                switch (item) {
+                    case "a":
+                    case "x":
+                        return "ax";
+                    case "b":
+                    case "y":
+                        return "by";
+                    case "c":
+                    case "z":
+                        return "cz";
+                    default:
+                        return item;
+                }
+            });
+            q.Enqueue("a");
+            q.Enqueue("b");
+            q.Enqueue("c");
+            q.Enqueue("x");
+            q.Enqueue("z");
+            q.Enqueue("w");
+            Assert.IsTrue(string.Join("", q) == "bxzw");
+        }
+    }
+}
