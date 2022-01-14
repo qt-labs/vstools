64e6c7b0d576b2e86ca3d41c36f42d2a2bf8ba44
Karsten Heimrich <karsten.heimrich@qt.io>
2022-01-14 16:22:15 +0100

    Implement templated Qt translation (.ts) file wizard
    Change-Id: I0efb2b66c5d7a9e9607b9334bea0212ed8d9cd16
    Reviewed-by: Miguel Costa <miguel.costa@qt.io>

---
+++ QtVsTools.Wizards/ItemWizard/Translation/TranslationPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿/****************************************************************************
+**
+** Copyright (C) 2022 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt VS Tools.
+**
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using QtVsTools.Wizards.Common;
+
+namespace QtVsTools.Wizards.ItemWizard
+{
+    public partial class TranslationPage : WizardPage
+    {
+        public TranslationPage()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Loaded += OnTranslationPageLoaded;
+        }
+
+        private void OnTranslationPageLoaded(object sender, RoutedEventArgs e)
+        {
+            var view = CollectionViewSource.GetDefaultView(LanguageListBox.ItemsSource);
+            view.Filter = obj =>
+            {
+                if (string.IsNullOrEmpty(searchBox.Text))
+                    return true;
+
+                var item = (KeyValuePair<string, string>)obj;
+                return item.Value.IndexOf(searchBox.Text, StringComparison.OrdinalIgnoreCase) >= 0;
+            };
+            LanguageListBox.SelectedIndex = 0;
+        }
+
+        private void OnSearchBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(LanguageListBox.ItemsSource).Refresh();
+            if (LanguageListBox.Items.Count == 1 || LanguageListBox.SelectedItem == null)
+                LanguageListBox.SelectedIndex = 0;
+        }
+
+        private void OnLanguageBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.RemovedItems != null && (e.AddedItems == null || e.AddedItems.Count == 0)) {
+                if (LanguageListBox.Items.Count != 0)
+                    LanguageListBox.SelectedIndex = 0;
+            }
+        }
+    }
+}
---
+++ QtVsTools.Wizards/ItemWizard/Translation/TranslationWizard.cs
@@ -0,0 +1,115 @@
+/****************************************************************************
+**
+** Copyright (C) 2022 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt VS Tools.
+**
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using EnvDTE;
+using QtVsTools.Common;
+using QtVsTools.Core;
+using QtVsTools.Wizards.Common;
+using QtVsTools.Wizards.ProjectWizard;
+
+namespace QtVsTools.Wizards.ItemWizard
+{
+    using static EnumExt;
+
+    public sealed class TsWizardData : WizardData
+    {
+        public string TsFile { get; set; }
+        public string CultureInfoName { get; set; }
+        public List<KeyValuePair<string, string>> CultureInfos { get; set; }
+    }
+
+    public sealed class TranslationWizard : ProjectTemplateWizard
+    {
+        protected override Options TemplateType => Options.ConsoleSystem | Options.GUISystem;
+
+        enum NewTranslationItem
+        {
+            [String("safeitemname")] SafeItemName,
+            [String("tsfilename")] TsFileName,
+            [String("cultureinfoname")] CultureInfoName
+        }
+
+        WizardData _WizardData;
+        protected override WizardData WizardData => _WizardData
+            ?? (_WizardData = new TsWizardData
+            {
+                DefaultModules = new List<string> { "QtCore"}
+            });
+
+        WizardWindow _WizardWindow;
+        protected override WizardWindow WizardWindow => _WizardWindow
+            ?? (_WizardWindow = new WizardWindow(title: "Qt Translation File Wizard")
+            {
+                new TranslationPage
+                {
+                    Data = WizardData,
+                    Header = @"Welcome to the Qt Translation File Wizard",
+                    Message = @"This wizard will add a new Qt empty translation file to your "
+                        + @"project. The wizard creates a .ts for the selected language.",
+                    PreviousButtonEnabled = false,
+                    NextButtonEnabled = false,
+                    FinishButtonEnabled = true,
+                    CancelButtonEnabled = true
+                },
+            });
+
+        protected override void BeforeWizardRun()
+        {
+            var tmp = WizardData as TsWizardData;
+            tmp.TsFile = Parameter[NewTranslationItem.SafeItemName];
+            tmp.CultureInfos = CultureInfo.GetCultures(CultureTypes.AllCultures)
+                .ToDictionary(
+                    mc => mc.Name.Replace("-", "_"),
+                    mc => mc.EnglishName,
+                    StringComparer.OrdinalIgnoreCase
+                ).OrderBy(item => item.Value).ToList();
+        }
+
+        protected override void BeforeTemplateExpansion()
+        {
+            var tmp = WizardData as TsWizardData;
+            Parameter[NewTranslationItem.CultureInfoName] = tmp.CultureInfoName;
+            Parameter[NewTranslationItem.TsFileName] = tmp.TsFile + "_" + tmp.CultureInfoName + ".ts";
+        }
+
+        protected override void Expand()
+        {
+            // do not call the base class method here
+        }
+
+        public override void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+            QtProject.AdjustWhitespace(Dte, projectItem.Properties.Item("FullPath").Value.ToString());
+        }
+    }
+}
