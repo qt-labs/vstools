ae6fd74dfde0073525ad27de66e3a6bca3d3f932
Karsten Heimrich <karsten.heimrich@qt.io>
2023-02-02 17:36:26 +0100

    Update and improve code, use new language features
    Change-Id: I943a00e0fad81bb3174f5dfa527d8712b1d54708
    Reviewed-by: Miguel Costa <miguel.costa@qt.io>

--- QtVsTools.Package/Package/Translation.cs
+++ QtVsTools.Package/Package/Translation.cs
@@ -1,31 +1,9 @@
-/****************************************************************************
-**
-** Copyright (C) 2022 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the Qt VS Tools.
-**
-** $QT_BEGIN_LICENSE:GPL-EXCEPT$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 as published by the Free Software
-** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
+/***************************************************************************************************
+ Copyright (C) 2023 The Qt Company Ltd.
+ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+***************************************************************************************************/
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -95,66 +73,58 @@ namespace QtVsTools
             RunTranslationTarget(BuildAction.Update, project);
         }
 
-        internal enum BuildAction { Update, Release }
+        private enum BuildAction { Update, Release }
 
-        static void RunTranslationTarget(
-            BuildAction buildAction,
-            EnvDTE.Project project,
+        private static void RunTranslationTarget(BuildAction buildAction, EnvDTE.Project project,
             IEnumerable<string> selectedFiles = null)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            using (WaitDialog.Start(
-                "Qt Visual Studio Tools", "Running translation tool...")) {
-
-                var qtPro = QtProject.Create(project);
-                if (project == null || qtPro == null) {
-                    Messages.Print(
-                        "translation: Error accessing project interface");
-                    return;
-                }
-
-                if (qtPro.FormatVersion < Resources.qtMinFormatVersion_Settings) {
-                    if (QtVsToolsPackage.Instance.Options.UpdateProjectFormat)
-                        Notifications.UpdateProjectFormat.Show();
-                    return;
-                }
-
-                var activeConfig = project.ConfigurationManager?.ActiveConfiguration;
-                if (activeConfig == null) {
-                    Messages.Print(
-                        "translation: Error accessing build interface");
-                    return;
-                }
-                var activeConfigId = string.Format("{0}|{1}",
-                    activeConfig.ConfigurationName, activeConfig.PlatformName);
-
-                var target = "QtTranslation";
-                var properties = new Dictionary<string, string>();
-                switch (buildAction) {
-                case BuildAction.Update:
-                    properties["QtTranslationForceUpdate"] = "true";
-                    break;
-                case BuildAction.Release:
-                    properties["QtTranslationForceRelease"] = "true";
-                    break;
-                }
-                if (selectedFiles != null)
-                    properties["SelectedFiles"] = string.Join(";", selectedFiles);
-
-                QtProjectBuild.StartBuild(
-                    project, project.FullName, activeConfigId, properties, new[] { target });
+            var qtPro = QtProject.Create(project);
+            if (project == null || qtPro == null) {
+                Messages.Print(
+                    "translation: Error accessing project interface");
+                return;
             }
+
+            if (qtPro.FormatVersion < Resources.qtMinFormatVersion_Settings) {
+                if (QtVsToolsPackage.Instance.Options.UpdateProjectFormat)
+                    Notifications.UpdateProjectFormat.Show();
+                return;
+            }
+
+            var activeConfig = project.ConfigurationManager?.ActiveConfiguration;
+            if (activeConfig == null) {
+                Messages.Print("translation: Error accessing build interface");
+                return;
+            }
+
+            using var _ = WaitDialog.Start("Qt Visual Studio Tools", "Running translation tool...");
+
+            var properties = new Dictionary<string, string>();
+            switch (buildAction) {
+            case BuildAction.Update:
+                properties["QtTranslationForceUpdate"] = "true";
+                break;
+            case BuildAction.Release:
+                properties["QtTranslationForceRelease"] = "true";
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(buildAction), buildAction, null);
+            }
+            if (selectedFiles != null)
+                properties["SelectedFiles"] = string.Join(";", selectedFiles);
+
+            var activeConfigId = $"{activeConfig.ConfigurationName}|{activeConfig.PlatformName}";
+            QtProjectBuild.StartBuild(
+                project, project.FullName, activeConfigId, properties, new[] { "QtTranslation" });
         }
 
         public static void RunlUpdate(EnvDTE.Solution solution)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            if (solution == null)
-                return;
-
-            foreach (var project in HelperFunctions.ProjectsInSolution(solution.DTE))
+            foreach (var project in HelperFunctions.ProjectsInSolution(solution?.DTE))
                 RunlUpdate(project);
         }
 
