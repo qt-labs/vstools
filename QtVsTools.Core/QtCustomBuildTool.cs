a55b883668c62fece0af5c3e30e0cb0ea57734c1
Karsten Heimrich <karsten.heimrich@qt.io>
2023-03-01 13:51:56 +0100

    Move classes into their own source file
    Change-Id: I986504e1d304bbb2e9bd9b2d72c53f81f7f2811c
    Reviewed-by: Miguel Costa <miguel.costa@qt.io>

---
+++ QtVsTools.Core/QtCustomBuildTool.cs
@@ -0,0 +1,60 @@
+/***************************************************************************************************
+ Copyright (C) 2023 The Qt Company Ltd.
+ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+***************************************************************************************************/
+
+using Microsoft.VisualStudio.VCProjectEngine;
+using QtVsTools.Core.QtMsBuild;
+
+namespace QtVsTools.Core
+{
+    internal class QtCustomBuildTool
+    {
+        private readonly QtMsBuildContainer qtMsBuild;
+        private readonly VCFileConfiguration vcConfig;
+        private readonly VCCustomBuildTool customTool;
+
+        private enum FileItemType { Other = 0, CustomBuild, QtMoc, QtRcc, QtRepc, QtUic }
+        private readonly FileItemType itemType = FileItemType.Other;
+
+        public QtCustomBuildTool(VCFileConfiguration config, QtMsBuildContainer container = null)
+        {
+            vcConfig = config;
+            qtMsBuild = container ?? new QtMsBuildContainer(new VcPropertyStorageProvider());
+
+            if (vcConfig?.File is VCFile vcFile) {
+                itemType = vcFile.ItemType switch
+                {
+                    "CustomBuild" => FileItemType.CustomBuild,
+                    QtMoc.ItemTypeName => FileItemType.QtMoc,
+                    QtRcc.ItemTypeName => FileItemType.QtRcc,
+                    QtRepc.ItemTypeName => FileItemType.QtRepc,
+                    QtUic.ItemTypeName => FileItemType.QtUic,
+                    _ => itemType
+                };
+            }
+            if (itemType == FileItemType.CustomBuild)
+                customTool = HelperFunctions.GetCustomBuildTool(vcConfig);
+        }
+
+        public string CommandLine => itemType switch
+        {
+            FileItemType.CustomBuild => customTool?.CommandLine ?? "",
+            FileItemType.QtMoc => qtMsBuild.GenerateQtMocCommandLine(vcConfig),
+            FileItemType.QtRcc => qtMsBuild.GenerateQtRccCommandLine(vcConfig),
+            FileItemType.QtRepc => qtMsBuild.GenerateQtRepcCommandLine(vcConfig),
+            FileItemType.QtUic => qtMsBuild.GenerateQtUicCommandLine(vcConfig),
+            _ => ""
+        };
+
+        public string Outputs => itemType switch
+        {
+            FileItemType.CustomBuild => customTool?.Outputs ?? "",
+            FileItemType.QtMoc => qtMsBuild.GetPropertyValue(vcConfig, QtMoc.Property.OutputFile),
+            FileItemType.QtRcc => qtMsBuild.GetPropertyValue(vcConfig, QtRcc.Property.OutputFile),
+            FileItemType.QtRepc => qtMsBuild.GetPropertyValue(vcConfig, QtRepc.Property.OutputFile),
+            FileItemType.QtUic => qtMsBuild.GetPropertyValue(vcConfig, QtUic.Property.OutputFile),
+            _ => ""
+        };
+    }
+}
---
+++ QtVsTools.Core/VcPropertyStorageProvider.cs
@@ -0,0 +1,139 @@
+/***************************************************************************************************
+ Copyright (C) 2023 The Qt Company Ltd.
+ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+***************************************************************************************************/
+
+using System.Collections.Generic;
+using Microsoft.VisualStudio.VCProjectEngine;
+using QtVsTools.Core.QtMsBuild;
+
+namespace QtVsTools.Core
+{
+    internal class VcPropertyStorageProvider : IPropertyStorageProvider
+    {
+        private string GetProperty(IVCRulePropertyStorage propertyStorage, string propertyName)
+        {
+            return propertyStorage?.GetUnevaluatedPropertyValue(propertyName) ?? "";
+        }
+
+        public string GetProperty(object obj, string itemType, string propertyName)
+        {
+            switch (obj) {
+            case VCFileConfiguration { Tool: IVCRulePropertyStorage propertyStorage }:
+                return GetProperty(propertyStorage, propertyName);
+            case VCConfiguration vcConfiguration: {
+                var ruleName = QtProject.GetRuleName(vcConfiguration, itemType);
+                return GetProperty(vcConfiguration.Rules.Item(ruleName) as IVCRulePropertyStorage,
+                    propertyName);
+            }
+            default:
+                return "";
+            }
+        }
+
+        private static bool SetProperty(IVCRulePropertyStorage storage, string name, string value)
+        {
+            if (storage?.GetUnevaluatedPropertyValue(name) != value)
+                storage?.SetPropertyValue(name, value);
+            return storage != null;
+        }
+
+        public bool SetProperty(object propertyStorage, string itemType, string propertyName,
+            string propertyValue)
+        {
+            switch (propertyStorage) {
+            case VCFileConfiguration { Tool: IVCRulePropertyStorage storage }:
+                return SetProperty(storage, propertyName, propertyValue);
+            case VCConfiguration vcConfiguration:
+                var ruleName = QtProject.GetRuleName(vcConfiguration, itemType);
+                return SetProperty(vcConfiguration.Rules.Item(ruleName) as IVCRulePropertyStorage,
+                    propertyName, propertyValue);
+            }
+            return false;
+        }
+
+        private static bool DeleteProperty(IVCRulePropertyStorage propertyStorage, string name)
+        {
+            propertyStorage?.DeleteProperty(name);
+            return propertyStorage != null;
+        }
+
+        public bool DeleteProperty(object propertyStorage, string itemType, string propertyName)
+        {
+            switch (propertyStorage) {
+            case VCFileConfiguration { Tool: IVCRulePropertyStorage storage }:
+                return DeleteProperty(storage, propertyName);
+            case VCConfiguration vcConfiguration:
+                var ruleName = QtProject.GetRuleName(vcConfiguration, itemType);
+                return DeleteProperty(vcConfiguration.Rules.Item(ruleName) as IVCRulePropertyStorage,
+                    propertyName);
+            }
+            return false;
+        }
+
+        public string GetConfigName(object propertyStorage)
+        {
+            return propertyStorage switch
+            {
+                VCFileConfiguration vcFileConfiguration => vcFileConfiguration.Name,
+                VCConfiguration vcConfiguration => vcConfiguration.Name,
+                _ => ""
+            };
+        }
+
+        public string GetItemType(object propertyStorage)
+        {
+            return propertyStorage is VCFileConfiguration { File: VCFile vcFile }
+                ? vcFile.ItemType : "";
+        }
+
+        public string GetItemName(object propertyStorage)
+        {
+            return propertyStorage is VCFileConfiguration { File: VCFile vcFile }
+                ? vcFile.Name : "";
+        }
+
+        public object GetParentProject(object propertyStorage)
+        {
+            return propertyStorage switch
+            {
+                VCFileConfiguration { ProjectConfiguration: VCConfiguration projectConfiguration }
+                    => projectConfiguration.project as VCProject,
+                VCConfiguration storage => storage.project as VCProject,
+                _ => null
+            };
+        }
+
+        public object GetProjectConfiguration(object project, string configName)
+        {
+            var vcProject = project as VCProject;
+            if (vcProject?.Configurations is not IVCCollection configurations)
+                return null;
+
+            foreach (VCConfiguration projConfig in configurations) {
+                if (projConfig.Name == configName)
+                    return projConfig;
+            }
+            return null;
+        }
+
+        public IEnumerable<object> GetItems(object project, string itemType, string configName = "")
+        {
+            var items = new List<VCFileConfiguration>();
+            var vcProject = project as VCProject;
+            if (vcProject?.GetFilesWithItemType(itemType) is not IVCCollection allItems)
+                return items;
+
+            foreach (VCFile vcFile in allItems) {
+                if (vcFile.FileConfigurations is not IVCCollection fileConfigurations)
+                    continue;
+                foreach (VCFileConfiguration fileConfiguration in fileConfigurations) {
+                    if (!string.IsNullOrEmpty(configName) && fileConfiguration.Name != configName)
+                        continue;
+                    items.Add(fileConfiguration);
+                }
+            }
+            return items;
+        }
+    }
+}
