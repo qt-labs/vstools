4d200336ec2c532019e72c3958fbf93d5cdba8f3
Miguel Costa <miguel.costa@qt.io>
2023-08-11 19:20:24 +0200

    Add utility functions
    These functions will be needed for the generation of project format
    conversion reports.
    
     * ToZipBase64: encode a string in UTF8, then compress (Deflate) the
    resulting buffer and return it as a Base64-encoded string.
    
     * FromZipBase64: decode a string generated with ToZipBase64 into the
    corresponding original content.
    
     * LastIndexOfArray: find the last occurrence of an array contained
    within another.
    
     * Hash: calculate the SHA256 of a given buffer, possibly augmented
    with an additional piece of text. This can be used as a crude way of
    'salting' the hash in order to secure it from possible replay exploits.
    
    Change-Id: I851746e980c575a97757a53df4ee4d1177744b3d
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

--- QtVsTools.Core/Common/Utils.cs
+++ QtVsTools.Core/Common/Utils.cs
@@ -4,6 +4,11 @@
 ***************************************************************************************************/
 
 using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace QtVsTools
 {
@@ -34,6 +39,68 @@ namespace QtVsTools
                 pos = index + oldValue.Length;
             }
             return result.Append(original, pos, original.Length - pos).ToString();
+        }
+
+        public static string ToZipBase64(this string text)
+        {
+            try {
+                var rawData = Encoding.UTF8.GetBytes(text);
+                using var zipData = new MemoryStream();
+                using (var encoder = new DeflateStream(zipData, CompressionLevel.Fastest, true)) {
+                    encoder.Write(BitConverter.GetBytes(text.Length), 0, sizeof(int));
+                    encoder.Write(rawData, 0, rawData.Length);
+                }
+                return Convert.ToBase64String(zipData.ToArray());
+            } catch (Exception) {
+                return string.Empty;
+            }
+        }
+
+        public static string FromZipBase64(string encodedText)
+        {
+            try {
+                using var zipData = new MemoryStream(Convert.FromBase64String(encodedText));
+                using var decoder = new DeflateStream(zipData, CompressionMode.Decompress);
+                var lengthData = new byte[sizeof(int)];
+                decoder.Read(lengthData, 0, sizeof(int));
+                var rawData = new byte[BitConverter.ToInt32(lengthData, 0)];
+                decoder.Read(rawData, 0, rawData.Length);
+                return Encoding.UTF8.GetString(rawData);
+
+            } catch (Exception) {
+                return string.Empty;
+            }
+        }
+
+        public static int LastIndexOfSpan<T>(this ReadOnlySpan<T> self, ReadOnlySpan<T> that)
+            where T : IEquatable<T>
+        {
+            if (that.IsEmpty || self.Length < that.Length)
+                return -1;
+            for (var i = self.Length - that.Length; i >= 0; --i) {
+                if (!self[i].Equals(that[0]))
+                    continue;
+                if (that.SequenceEqual(self.Slice(i, that.Length)))
+                    return i;
+            }
+            return -1;
+        }
+
+        public static int LastIndexOfArray<T>(this T[] self, T[] that)
+            where T : IEquatable<T>
+        {
+            return LastIndexOfSpan<T>(self, that);
+        }
+
+        public static byte[] Hash(byte[] data, int index = 0, int count = -1, string text = "сору")
+        {
+            using var sha256 = SHA256.Create();
+            using var dataRaw = new MemoryStream();
+            using var dataUtf8 = new StreamWriter(dataRaw, Encoding.UTF8) { AutoFlush= true };
+            dataRaw.Write(data, index, count switch { < 0 => data.Length, _ => count });
+            dataUtf8.Write(text);
+            dataRaw.Seek(0, SeekOrigin.Begin);
+            return sha256.ComputeHash(dataRaw);
         }
     }
 }
---
+++ Tests/Test_QtVsTools.Core/Test_Utils.cs
@@ -0,0 +1,54 @@
+/***************************************************************************************************
+ Copyright (C) 2023 The Qt Company Ltd.
+ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+***************************************************************************************************/
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QtVsTools.Test.Core
+{
+    [TestClass]
+    public class Test_Utils
+    {
+        private double NanosecondsPerTick => Math.Pow(10.0, 9.0) / Stopwatch.Frequency;
+
+        [TestMethod]
+        public void Test_LastIndexOfArray()
+        {
+            Assert.AreEqual(-1, Array.Empty<int>().LastIndexOfArray(Array.Empty<int>()));
+            Assert.AreEqual(-1, Array.Empty<int>().LastIndexOfArray(new[] { 1, 2, 3 }));
+            Assert.AreEqual(-1, new[] { 1, 2, 3 }.LastIndexOfArray(Array.Empty<int>()));
+            Assert.AreEqual(-1, new[] { 1, 2, 3 }.LastIndexOfArray(new[] { 1, 2, 3, 4 }));
+
+            var text = "the quick brown fox jumped over the lazy dog";
+            var haystack = text.ToArray();
+            var find = (string what) =>
+            {
+                var needle = what.ToArray();
+                var time = Stopwatch.StartNew();
+                var index = haystack.LastIndexOfArray(needle);
+                time.Stop();
+                Assert.IsTrue(index == text.LastIndexOf(what));
+                return time.ElapsedTicks;
+            };
+
+            var test = new[] { "dog", "fox", "cat", "the", text }
+                .ToDictionary(x => x, x => 0.0);
+            var testRepeats = 100000;
+            var testCases = test.Keys.ToList();
+            foreach (var _ in Enumerable.Range(0, testRepeats))
+                testCases.ForEach(x => test[x] += find(x));
+
+            var min = test.Values.Min() / testRepeats;
+            var avg = test.Values.Sum() / (test.Count * testRepeats);
+            var max = test.Values.Max() / testRepeats;
+            Debug.WriteLine($"  * Best case...{min * NanosecondsPerTick:F0} nsecs");
+            Debug.WriteLine($"  * Average.....{avg * NanosecondsPerTick:F0} nsecs");
+            Debug.WriteLine($"  * Worst case..{max * NanosecondsPerTick:F0} nsecs");
+        }
+    }
+}
