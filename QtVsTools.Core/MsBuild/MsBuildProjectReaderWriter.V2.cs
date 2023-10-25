qt-labs/vstools

commit 9405f497794a81fa207dc214f4f4e8e2faadc895

Amend a548ea, filter the list of Qt modules used in the project
By first adding all modules from all known Qt versions (currently
Qt5 and Qt6), it can happen that we have values for specific modules
(namely webengine (Qt5) and webenginecore(Qt6)) duplicated in the
list. In the end, we need to filter this list once more to truly add
only the modules provided by the Qt version.

Fixes: QTVSADDINBUG-1126
Fixes: QTVSADDINBUG-1130
Change-Id: I7905f56616336c2d81bb86a7443c023cf7323151
Reviewed-by: Miguel Costa <miguel.costa@qt.io>

QtVsTools.Core/MsBuild/MsBuildProjectReaderWriter.V2.cs
@@ -19,7 +19,7 @@ namespace QtVsTools.Core.MsBuild
     {
         private bool UpgradeFromV2()
         {
-            var defaultVersionName = QtVersionManager.The().GetDefaultVersion();
+            var qtInstallValue = QtVersionManager.The().GetDefaultVersion();
 
             // Get project user properties (old format)
             var userProps = this[Files.Project].Xml
@@ -37,7 +37,6 @@ namespace QtVsTools.Core.MsBuild
                 .ToList()
                 .ForEach(config =>
                 {
-                    var qtInstallValue = defaultVersionName;
                     if (userProps != null) {
                         string platform = null;
                         try {
@@ -163,6 +162,13 @@ namespace QtVsTools.Core.MsBuild
                     .Where(x => !moduleDefines.Contains(x))));
             }
             Commit("Removing Qt module macros from resource compiler properties");
+
+            var qtVersion = QtVersionManager.The().GetVersionInfo(qtInstallValue);
+            moduleNames = moduleNames // remove proVarQT values not provided by the used Qt version
+                .Where(moduleName => QtModules.Instance.GetAvailableModules(qtVersion.qtMajor)
+                    .Select(module => module.proVarQT)
+                    .Contains(moduleName))
+                .ToHashSet();
 
             this[Files.Project].Xml
                 .Elements(ns + "Project")

commit f6be3a87183d2e49b2fcfb72446bbd37c8418708

Fix private modules discarded in .pro import
Issue was introduced by 9405f497.

Fixes: QTVSADDINBUG-1137
Change-Id: I86aac9bbe5fbac980ef929049fdee17b1b9166c2
Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

QtVsTools.Core/MsBuild/MsBuildProjectReaderWriter.V2.cs
@@ -172,10 +172,11 @@ namespace QtVsTools.Core.MsBuild
             Commit("Removing Qt module macros from resource compiler properties");
 
             var qtVersion = QtVersionManager.The().GetVersionInfo(qtInstallValue);
-            moduleNames = moduleNames // remove proVarQT values not provided by the used Qt version
-                .Where(moduleName => QtModules.Instance.GetAvailableModules(qtVersion.qtMajor)
-                    .Select(module => module.proVarQT)
-                    .Contains(moduleName))
+            moduleNames = QtModules.Instance.GetAvailableModules(qtVersion.qtMajor)
+                // remove proVarQT values not provided by the used Qt version
+                .SelectMany(x => x.proVarQT.Split(' '))
+                .SelectMany(x => x.EndsWith("-private") ? new[] { x } : new[] { x, $"{x}-private" })
+                .Intersect(moduleNames)
                 .ToHashSet();
 
             this[Files.Project].Xml