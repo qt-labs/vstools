b6e902017fbdce55836fcf5ff16c1ef01f399824
Miguel Costa <miguel.costa@qt.io>
2023-12-20 15:33:18 +0100

    Make buildtime properties available to any project
    To add this feature to a project, add EvalProps.cs and import
    EvalProps.targets at the end of the project file.
    
    Change-Id: I8302df3859da8519dbc25f248b6996cdf7926085
    Reviewed-by: Ali Can Demiralp <ali.demiralp@qt.io>
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

--- EvalProps.cs
+++ EvalProps.cs
@@ -7,17 +7,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 
-namespace QtVsTools.Test.QtMsBuild.Build
+namespace QtVsTools
 {
     public static class Properties
     {
-        static Dictionary<string, string> PropEval = File.ReadAllLines("PropEval.csv")
+        static Dictionary<string, string> PropEval = File.ReadAllLines("evalprops.csv")
             .Select(x => x.Split(';'))
             .ToDictionary(x => x.First(), x => string.Join(";", x.Skip(1)));
         public static IReadOnlyDictionary<string, string> ValueOf => PropEval;
+        public static string Configuration => ValueOf["Configuration"];
         public static string MSBuildToolsPath => ValueOf["MSBuildToolsPath"];
+        public static string SolutionDir => ValueOf["SolutionDir"];
         public static string VsInstallRoot => ValueOf["VsInstallRoot"];
         public static string VCTargetsPath => ValueOf["VCTargetsPath"];
-        public static string SolutionDir => ValueOf["SolutionDir"];
     }
 }
