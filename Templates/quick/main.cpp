e5a06c482478984a252aa905b418c516d060d91e
Ali Can Demiralp <ali.demiralp@qt.io>
2024-02-19 14:59:07 +0100

    Prevent deprecation warning / error in Qt quick application template
    Prevent C4996: 'Qt::AA_EnableHighDpiScaling': High-DPI scaling is always
    enabled. This attribute no longer has any effect.
    
    Fixes: QTVSADDINBUG-1178
    Change-Id: I1d2d308662a63d6b4348f6bdec8f747fc731d6c5
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

--- Templates/quick/main.cpp
+++ Templates/quick/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-#if defined(Q_OS_WIN)
+#if defined(Q_OS_WIN) && QT_VERSION_CHECK(5, 6, 0) <= QT_VERSION && QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 
