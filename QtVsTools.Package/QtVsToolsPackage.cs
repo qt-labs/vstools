aac5756430631e7a9b2211c7be5f57a7bbd391d6
Karsten Heimrich <karsten.heimrich@qt.io>
2024-06-18 13:01:54 +0200

    Remove superfluous options page property
    After introducing the static settings class in 91df5f this code is
    no longer in use.
    
    Change-Id: I9fdeddc9b3c62327636451c15feb2165250a18a8
    Reviewed-by: Ali Can Demiralp <ali.demiralp@qt.io>
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

--- QtVsTools.Package/QtVsToolsPackage.cs
+++ QtVsTools.Package/QtVsToolsPackage.cs
@@ -24,7 +24,6 @@ using static Microsoft.VisualStudio.Shell.PackageAutoLoadFlags;
 
 namespace QtVsTools
 {
-    using Common;
     using Core;
     using Core.Options;
     using Package;
@@ -94,15 +93,7 @@ namespace QtVsTools
 
     public sealed class QtVsToolsPackage : AsyncPackage, IVsServiceProvider
     {
-        LazyFactory Lazy { get; } = new();
-
         public DTE Dte { get; private set; }
-        public QtOptionsPage Options => Lazy.Get(() => Options, () =>
-            ThreadHelper.JoinableTaskFactory.Run(async () =>
-            {
-                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-                return GetDialogPage(typeof(QtOptionsPage)) as QtOptionsPage;
-            }));
 
         public Editors.QtDesigner QtDesigner { get; private set; }
         public Editors.QtLinguist QtLinguist { get; private set; }
