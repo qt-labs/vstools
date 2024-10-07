ad2b993b163af35c16234cd7f360257cdaa4bb12
Robert Löhning <robert.loehning@qt.io>
2024-10-07 12:52:27 +0200

    Squish: Use correct object for "Services.SignIn.WizardFrame"
    Change-Id: Idb9d813f5d7006771e1ea5efffb0f4cbb58336dd
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

--- Tests/system/shared/globalnames.py
+++ Tests/system/shared/globalnames.py
@@ -14,9 +14,14 @@ file_Exit_MenuItem = {"container": file_MenuItem, "text": "Exit", "type": "MenuI
 extensions_MenuItem = {"container": microsoft_Visual_Studio_MenuBar, "text": "Extensions", "type": "MenuItem"}
 extensions_Qt_VS_Tools_MenuItem = {"container": extensions_MenuItem, "text": "Qt VS Tools",
                                    "type": "MenuItem"}
-msvs_Skip_this_for_now_Button = {"container": microsoft_Visual_Studio_Window, "text": "Skip this for now.", "type": "Button"}
-msvs_Start_Visual_Studio_Button = {"container": microsoft_Visual_Studio_Window, "text": "Start Visual Studio", "type": "Button"}
-msvs_Not_now_maybe_later_Label = {"container": microsoft_Visual_Studio_Window, "text": "Not now, maybe later.", "type": "Label"}
+msvs_Services_SignIn_WizardFrame = {"class": "Microsoft.VisualStudio.Services.SignIn.WizardFrame",
+                                    "text": "Microsoft Visual Studio", "type": "Window"}
+msvs_Skip_this_for_now_Button = {"container": msvs_Services_SignIn_WizardFrame,
+                                 "text": "Skip this for now.", "type": "Button"}
+msvs_Start_Visual_Studio_Button = {"container": msvs_Services_SignIn_WizardFrame,
+                                   "text": "Start Visual Studio", "type": "Button"}
+msvs_Not_now_maybe_later_Label = {"container": msvs_Services_SignIn_WizardFrame,
+                                  "text": "Not now, maybe later.", "type": "Label"}
 Initializing_MenuItem = {"text": RegularExpression(".*Initializing...\\s*$"), "type": "MenuItem"}
 pART_Popup_New_MenuItem = {"text": "New", "type": "MenuItem"}
 pART_Popup_Project_MenuItem = {"text": "Project...", "type": "MenuItem"}
