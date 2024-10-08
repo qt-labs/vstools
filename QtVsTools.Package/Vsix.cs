5038e31170303079789a4d84c7c60be2f4376e5e
Karsten Heimrich <karsten.heimrich@qt.io>
2024-10-08 14:12:56 +0200

    Update package description
    * Emphasize the official status of the extensions
    
    Change-Id: Ib742c573d9f9896dade5ac2e2c0200b6ad3b22d9
    Reviewed-by: Miguel Costa <miguel.costa@qt.io>

--- QtVsTools.Package/Vsix.cs
+++ QtVsTools.Package/Vsix.cs
@@ -14,7 +14,7 @@ namespace QtVsTools.Package
         public const string Author = "The Qt Company Ltd.";
 
         /// <summary>The description of the extension.</summary>
-        public const string Description = "The Qt VS Tools for Visual Studio 2019 allow developers to use the standard development environment without having to worry about any Qt-related build steps or tools.";
+        public const string Description = "This official Qt Group extension, Qt Visual Studio Tools, integrates Qt development tools into Microsoft Visual Studio 2019, enabling developers to use their familiar Windows environment without worrying about Qt-related build steps or tools.";
 
         /// <summary>The extension identifier.</summary>
         public const string Id = "QtVsTools.bf3c71c0-ab41-4427-ada9-9b3813d89ff5";
