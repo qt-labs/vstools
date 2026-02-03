976aeb696727569b1da895e1fb49be5e38f2d990
Karsten Heimrich <karsten.heimrich@qt.io>
2026-02-03 13:01:42 +0100

    Introduce MsBuild.GetProperty(...) for dynamic property retrieval
    Change-Id: Ic50cba025221b92992179919116112d076ebc148
    Reviewed-by: Miguel Costa <miguel.costa@qt.io>

--- Tests/Test_QtMsBuild.Build/MsBuild.cs
+++ Tests/Test_QtMsBuild.Build/MsBuild.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 The Qt Company Ltd.
+// Copyright (C) 2026 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 using System;
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Xml;
+using System.Text;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
 
@@ -164,6 +164,80 @@ namespace QtVsTools.Test.QtMsBuild.Build
             msbuildProc.BeginErrorReadLine();
             msbuildProc.WaitForExit();
             return msbuildProc.ExitCode == 0;
+        }
+
+        public static string GetProperty(string workDir, string projectPath, string propertyName,
+            params string[] args)
+        {
+            if (string.IsNullOrWhiteSpace(projectPath))
+                throw new ArgumentNullException(nameof(projectPath));
+            if (string.IsNullOrWhiteSpace(propertyName))
+                throw new ArgumentNullException(nameof(propertyName));
+
+            var allArgs = new List<string>
+            {
+                projectPath,
+                "-nologo",
+                "-verbosity:quiet",
+                $"-getProperty:{propertyName}"
+            };
+            if (args is { Length: > 0 })
+                allArgs.AddRange(args);
+
+            var result = RunWithOutput(workDir, allArgs.ToArray());
+            if (result.ExitCode == 0)
+                return result.StdOut.Trim(' ', '\r', '\n', '\t');
+
+            // we should not end up here in normal circumstances
+            throw new InvalidOperationException($"msbuild -getProperty:{propertyName} "
+                + $"failed: {result.StdErr}{result.StdOut}");
+        }
+
+        private static (int ExitCode, string StdOut, string StdErr) RunWithOutput(
+            string workDir, params string[] args)
+        {
+            var stdOut = new StringBuilder();
+            var stdErr = new StringBuilder();
+            var msbuildStartInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                FileName = Path.Combine(Properties.MSBuildToolsPath, "MSBuild.exe"),
+                Arguments = string.Join(" ", args
+                    .Where(arg => arg is { Length: > 0 })
+                    .Select(arg => arg.Contains(" ") ? $"\"{arg}\"" : arg)),
+                WorkingDirectory = workDir,
+                EnvironmentVariables =
+                {
+                    ["VsInstallRoot"] = Properties.VsInstallRoot,
+                    ["VCTargetsPath"] = Properties.VCTargetsPath,
+                    ["QtMsBuild"] = GlobalProperties["QtMsBuild"]
+                }
+            };
+
+            var msbuildProc = new Process
+            {
+                StartInfo = msbuildStartInfo
+            };
+            msbuildProc.OutputDataReceived += (_, ev) =>
+            {
+                if (ev.Data != null)
+                    stdOut.AppendLine(ev.Data);
+            };
+            msbuildProc.ErrorDataReceived += (_, ev) =>
+            {
+                if (ev.Data != null)
+                    stdErr.AppendLine(ev.Data);
+            };
+            if (!msbuildProc.Start())
+                throw new InvalidOperationException("Could not start MSBuild.exe.");
+            msbuildProc.BeginOutputReadLine();
+            msbuildProc.BeginErrorReadLine();
+            msbuildProc.WaitForExit();
+
+            return (msbuildProc.ExitCode, stdOut.ToString(), stdErr.ToString());
         }
     }
 }
